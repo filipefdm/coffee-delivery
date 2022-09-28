b228e59018952bdcc9b11ee2b4667c704612e735
filipefdm <femotta96@gmail.com>
2022-09-28 12:53:36 -0300

    :sparkles: feat: create complete order page

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,3 +1,67 @@
+import { CompleteOrderForm } from "./components/CompleteOrderForm";
+import { SelectedCoffees } from "./components/SelectedCoffees";
+
+import { CompleteOrderContainer } from "./styles";
+
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm, FormProvider } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
+
+enum PaymentMethods {
+  credit = "credit",
+  debit = "debit",
+  money = "money",
+}
+
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, "Informe o CEP"),
+  street: zod.string().min(1, "Informe a rua"),
+  number: zod.string().min(1, "Informe o número"),
+  complement: zod.string(),
+  district: zod.string().min(1, "Informe o bairro"),
+  city: zod.string().min(1, "Informe a cidade"),
+  uf: zod.string().min(1, "Informe a UF"),
+  paymentMethod: zod.nativeEnum(PaymentMethods, {
+    errorMap: () => {
+      return { message: "Informe o método de pagamento" };
+    },
+  }),
+});
+
+export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>;
+
+type ConfirmOrderFormData = OrderData;
+
 export function CompleteOrderPage() {
-  return <h1>Complete</h1>;
+  const confirmOrderForm = useForm<ConfirmOrderFormData>({
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+  });
+
+  const { handleSubmit } = confirmOrderForm;
+
+  const navigate = useNavigate();
+
+  const { cleanCart } = useCart();
+
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    navigate("/orderConfirmed", {
+      state: data,
+    });
+    cleanCart();
+  }
+
+  return (
+    <FormProvider {...confirmOrderForm}>
+      <CompleteOrderContainer
+        className="container"
+        onSubmit={handleSubmit(handleConfirmOrder)}
+      >
+        <CompleteOrderForm />
+        <SelectedCoffees />
+      </CompleteOrderContainer>
+    </FormProvider>
+  );
 }
