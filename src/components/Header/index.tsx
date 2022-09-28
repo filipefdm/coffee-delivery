1c79e5af39d67635ad107350969d851328c4dd4f
filipefdm <femotta96@gmail.com>
2022-09-28 12:51:31 -0300

    :recycle: refactor: add useCart hook, nav link to home and complete order & show cart quantities

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,21 +5,31 @@ import {
 } from "./styles";
 import coffeeLogoImg from "../../assets/coffee-delivery-logo.svg";
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
 
 export function Header() {
+  const { cartQuantity } = useCart();
+
   return (
     <HeaderContainer>
       <div className="container">
-        <img src={coffeeLogoImg} alt="" />
+        <NavLink to="/">
+          <img src={coffeeLogoImg} alt="" />
+        </NavLink>
 
         <HeaderButtonsContainer>
           <HeaderButton variant="purple">
             <MapPin size={20} weight="fill" />
             Porto Alegre, RS
           </HeaderButton>
-          <HeaderButton variant="yellow">
-            <ShoppingCart size={20} weight="fill" />
-          </HeaderButton>
+
+          <NavLink to="/completeOrder">
+            <HeaderButton variant="yellow">
+              {cartQuantity >= 1 && <span>{cartQuantity}</span>}
+              <ShoppingCart size={20} weight="fill" />
+            </HeaderButton>
+          </NavLink>
         </HeaderButtonsContainer>
       </div>
     </HeaderContainer>
